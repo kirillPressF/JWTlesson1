125583a902bb5d6cb0bd5611513e1ba1342ec5be
kirillPressF <prohorenko25@gmail.com>
2024-03-22 23:06:52 +0200

    Добавьте файлы проекта.

---
+++ JWTlesson1/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Test API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+builder.Services.AddProblemDetails();
+builder.Services.AddApiVersioning();
+builder.Services.AddRouting(x=>x.LowercaseUrls = true);
+
+// add db context
+//Move connection string to user secrets for a real app
+
+builder.Services.AddDbContext<JWTlesson1Dbcontext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(JWTlesson1Dbcontext)));
+});
